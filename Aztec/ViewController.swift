10c66019301d7ac03e0ffe2ca112614def268bde
Andrea Gottardo <andrea@gottardo.me>
2018-04-22 12:19:44 -0700

    Initial Commit

---
+++ Aztec/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aztec
+//
+//  Created by Andrea Gottardo on 22/04/2018.
+//  Copyright © 2018 Andrea Gottardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
