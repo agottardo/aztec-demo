9fc7955e4feda42205d72b130342fdf9fd4539d2
Andrea Gottardo <andrea@gottardo.me>
2018-12-31 11:28:26 +0100

    Update to current version

--- Aztec/ViewController.swift
+++ Aztec/ViewController.swift
@@ -7,17 +7,47 @@
 //
 
 import UIKit
+import CryptoSwift
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var aztecImageView: UIImageView!
+    
+    @IBOutlet weak var msgLabel: UILabel!
+    
+    var timer : Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tock()
+        timer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.tock), userInfo: nil, repeats: true)
+    }
+    
+    @objc func tock() {
+        let encoded = encodedTicketString(msg: "15436#123456789123456789#")
+        let image = encodingToAztecImage(encoding: encoded.data(using: .ascii)! as NSData)
+        aztecImageView.image = image
+        msgLabel.text = encoded
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func encodedTicketString(msg: String) -> String {
+        let salt = "HelloWorld".uppercased()
+        let time = String(Int(Date().timeIntervalSince1970))
+        let crypto : String = msg + time + salt
+        let bytes: Array<UInt8> = crypto.bytes
+        return msg + time + " " + bytes.sha3(SHA3.Variant.keccak512).toBase64()!
+    }
+    
+    func encodingToAztecImage(encoding: NSData) -> UIImage {
+        let filter = CIFilter(name: "CIAztecCodeGenerator")
+        filter?.setValue(encoding, forKey: "inputMessage")
+        let transform = CGAffineTransform(scaleX: 20.0, y: 20.0) // Scale by 5 times along both dimensions
+        let output = filter!.outputImage?.transformed(by: transform)
+        return UIImage(ciImage: output!)
     }
 
 
---
+++ Pods/Target Support Files/Pods-Aztec/Pods-Aztec-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Aztec : NSObject
+@end
+@implementation PodsDummy_Pods_Aztec
+@end
